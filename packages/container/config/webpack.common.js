b30b7f8f57496848035a3f0482ebfd5a742a65e9
Bilal El Mrabate <bilal_el_mrabate@epam.com>
2022-10-04 09:27:52 +0200

    Initial commit

---
+++ packages/container/config/webpack.common.js
@@ -0,0 +1,24 @@
+module.exports = {
+  module: {
+    rules: [
+      // here we define a loader
+      // The goal of a loader is to tell webpack to process some different files as we start to import them into our project
+      // The first loader we're going to wire up is babel,
+      // babel is going to be in charge of processing all of our code from the ES20, ES15... and into into es5 code
+      {
+        // Whenver we import a file with the extension of js or mjs we want it to be processed by Babel
+        test: /\.m?js$/,
+        // Do not try to run Babel on an file from node_modules directory
+        exclude: /node_modules/,
+        //
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime'],
+          },
+        },
+      },
+    ],
+  },
+};
