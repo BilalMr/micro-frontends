7607ba205a8df6f118f160dfc9803bcd07387c33
Bilal El Mrabate <bilal_el_mrabate@epam.com>
2022-10-04 10:48:48 +0200

    creating container deploy script

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   module: {
     rules: [
@@ -21,4 +22,9 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
 };
