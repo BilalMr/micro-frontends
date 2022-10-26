bca8533104f06a85f7071c98e605ae370817ecf5
Bilal El Mrabate <bilal_el_mrabate@epam.com>
2022-10-26 12:34:20 +0200

    adds marketing deployment

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -7,14 +7,18 @@ const domain = process.env.PRODUCTION_DOMAIN;
 
 const prodConfig = {
   mode: 'production',
+  // This ensure that whenever we build some files for production, all different files are built will use
+  // the template [name].[contenthash].js, this is done primarily for cashing issues
   output: {
     filename: '[name].[contenthash].js',
+    // the html plugin will take all the names of the files and prepend them with /container/latest/
     publicPath: '/container/latest/',
   },
   plugins: [
     new ModuleFederationPlugin({
       name: 'container',
       remotes: {
+        // Here we made a big assumption that the remote entry files for our marketing is going to be nested inside marketing
         marketing: `marketing@${domain}/marketing/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
